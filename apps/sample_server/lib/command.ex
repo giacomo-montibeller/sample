8bc2e03c8967008a333de41474e79c5c15246cac
Giacomo Montibeller <giacomo.montibeller@xpeppers.com>
2020-03-05 12:38:10 +0100

    add doctests

---
+++ apps/sample_server/lib/command.ex
@@ -0,0 +1,40 @@
+defmodule SampleServer.Command do
+  @doc ~S"""
+    Parses the given `line` into a command.
+
+    ## Examples
+
+        iex> SampleServer.Command.parse "CREATE shopping\r\n"
+        {:ok, {:create, "shopping"}}
+
+        iex> SampleServer.Command.parse "CREATE  shopping  \r\n"
+        {:ok, {:create, "shopping"}}
+
+        iex> SampleServer.Command.parse "PUT shopping milk 1\r\n"
+        {:ok, {:put, "shopping", "milk", "1"}}
+
+        iex> SampleServer.Command.parse "GET shopping milk\r\n"
+        {:ok, {:get, "shopping", "milk"}}
+
+        iex> SampleServer.Command.parse "DELETE shopping eggs\r\n"
+        {:ok, {:delete, "shopping", "eggs"}}
+
+    Unknown commands or commands with the wrong number of
+    arguments return an error:
+
+        iex> SampleServer.Command.parse "UNKNOWN shopping eggs\r\n"
+        {:error, :unknown_command}
+
+        iex> SampleServer.Command.parse "GET shopping\r\n"
+        {:error, :unknown_command}
+  """
+  def parse(line) do
+    case String.split(line) do
+      ["CREATE", bucket] -> {:ok, {:create, bucket}}
+      ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
+      ["PUT", bucket, key, value] -> {:ok, {:put, bucket, key, value}}
+      ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
+      _ -> {:error, :unknown_command}
+    end
+  end
+end
---
+++ apps/sample_server/test/command_test.exs
@@ -0,0 +1,4 @@
+defmodule SampleServer.CommandTest do
+  use ExUnit.Case, async: true
+  doctest(SampleServer.Command)
+end
