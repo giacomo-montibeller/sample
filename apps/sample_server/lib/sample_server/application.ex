f5dd03b4fc09e191d9e1ee41509f474af07b195c
Giacomo Montibeller <giacomo.montibeller@xpeppers.com>
2020-03-05 09:53:03 +0100

    add sample server

--- apps/sample_server/lib/sample_server/application.ex
+++ apps/sample_server/lib/sample_server/application.ex
@@ -1,18 +1,14 @@
 defmodule SampleServer.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
     children = [
-      # Starts a worker by calling: SampleServer.Worker.start_link(arg)
-      # {SampleServer.Worker, arg}
+      {Task.Supervisor, name: Sample.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> SampleServer.accept(port) end}, restart: :permanent)
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SampleServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- apps/sample_server/test/sample_server_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SampleServerTest do
-  use ExUnit.Case
-  doctest SampleServer
-
-  test "greets the world" do
-    assert SampleServer.hello() == :world
-  end
-end
